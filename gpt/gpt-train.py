a6deae0ba1611ca3c718a9faa093ae8ab3297880
MunirAbobaker <munir.abobaker@sap.com>
2024-08-26 17:47:24 +0200

    transformer block wiht mlp and mattn

--- gpt/gpt-train.py
+++ gpt/gpt-train.py
@@ -47,6 +47,35 @@ class CasualSelfAttention(nn.Module):
 
         y = y.transpose(1, 2).contiguous().view(B, T, C)    
         return y
+    
+    # feed forward network
+    
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        self.n_embd = config.n_embd
+        self.fc1 = nn.Linear(self.n_embd, 4 * self.n_embd)
+        self.affn = nn.GELU()   
+        self.c_proj = nn.Linear(4 * self.n_embd, self.n_embd)
+
+    def forward(self, x):
+        x = self.affn(self.fc1(x))
+        x = self.c_proj(x)
+        return x
+        
+# block
+class Block(nn.Module):
+
+    def __init__(self, config):
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
 
 
 
